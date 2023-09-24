11de1bd0afd055dcd2f45653fb78681d0e01026e
Joshua Torrington-Smith <59626141+CatSandwich@users.noreply.github.com>
2023-09-24 16:12:26 -0400

    Removed problematic Object serialization

--- Assets/Scripts/States/MainState.cs
+++ Assets/Scripts/States/MainState.cs
@@ -6,16 +6,14 @@ namespace States
     [CreateAssetMenu(fileName = "Main State", menuName = "States/Main")]
     public class MainState : GameStateMachine.State
     {
-        public Object MainScene;
-        
         public override void OnEnter(GameStateMachine machine)
         {
-            SceneManager.LoadSceneAsync(MainScene.name, LoadSceneMode.Additive);
+            SceneManager.LoadSceneAsync("Main", LoadSceneMode.Additive);
         }
 
         public override void OnExit()
         {
-            SceneManager.UnloadSceneAsync(MainScene.name);
+            SceneManager.UnloadSceneAsync("Main");
         }
     }
 }
