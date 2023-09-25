969d4a1e7ffd8f9bc34dbf499273bf43a7a52ef5
Joshua Torrington-Smith <59626141+CatSandwich@users.noreply.github.com>
2023-09-24 22:48:24 -0400

    Leaderboard UI and completes flow loop

---
+++ Assets/Scripts/Leaderboard/UI/LeaderboardEntry.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+namespace Leaderboard.UI
+{
+    public class LeaderboardEntry : MonoBehaviour
+    {
+        public TextMeshProUGUI Name;
+        public TextMeshProUGUI Score;
+    }
+}
