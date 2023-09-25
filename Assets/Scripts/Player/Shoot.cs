e5552cc24369beb9f1650ea0e440bce4625c6ab7
Frautcres <Frautcres@FRAUTCRES>
2023-09-24 20:39:59 -0400

    Audio - Shooting and recharge.

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -8,7 +8,15 @@ namespace Player
     {
         public RaycastDamageSender Sender;
         public Material shootMaterial;
-        public float shootAnimationTime = 0.4f;
+        public float shootAnimationTime = 0.4f; //This should probably be constant - Evan
+        public const float SHOOT_RECHARGE_TIME = 1f;
+
+        //AUDIO
+        public AudioSource shootSource;
+        public AudioClip[] shootClips;
+        public AudioSource rechargeSource;
+        public AudioClip[] rechargeClips;
+        private const double RECHARGE_OFFSET = 0.72925d; //No time for audio metadata
 
         float lastShootTime = 0;
         bool duringAnimation = false;
@@ -18,11 +26,28 @@ namespace Player
             while (true)
             {
                 yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+
+                //We are now shooting
                 lastShootTime = Time.time;
                 duringAnimation = true;
 				Vector3 click = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                
+                //Audio
+                if (shootSource != null && shootClips.Length > 0) //serialized arrays won't be null.
+                {
+                    shootSource.clip = shootClips[Random.Range(0, shootClips.Length)];
+                    shootSource.Play(); 
+                }
+                if (rechargeSource != null && rechargeClips.Length > 0)
+				{
+                    rechargeSource.clip = rechargeClips[Random.Range(0, rechargeClips.Length)];
+                    rechargeSource.PlayScheduled(AudioSettings.dspTime + SHOOT_RECHARGE_TIME - RECHARGE_OFFSET);
+				}
+                
+                //Raycast
                 Sender.Raycast(click - transform.position);
-                yield return new WaitForSeconds(1f);
+                yield return new WaitForSeconds(SHOOT_RECHARGE_TIME);
+
             }
         }
 
