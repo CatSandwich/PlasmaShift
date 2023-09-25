4dfbbb9437216eb32e24808c5dd82ed4b88f1c83
Frautcres <Frautcres@FRAUTCRES>
2023-09-25 13:00:47 -0400

    add shift scripts and sounds

--- Assets/Scripts/Player/Shift.cs
+++ Assets/Scripts/Player/Shift.cs
@@ -6,6 +6,13 @@ namespace Player
 {
 	public class Shift : MonoBehaviour
 	{
+		public AudioSource shiftSource;
+		public AudioClip[] shiftClips;
+		public AudioSource shiftRechargeSource;
+		public AudioClip[] shiftRechargeClips;
+
+		private const double RECHARGE_SOUND_MARKER = 0.99571428571d;
+
 		public float cooldown = 3;
 		public float distance = 3;
 
@@ -17,6 +24,20 @@ namespace Player
 			while (true)
 			{
 				yield return new WaitUntil(() => Input.GetKey(KeyCode.Space));
+
+				//Do audio
+				if (shiftSource && shiftClips.Length > 0)
+				{
+					shiftSource.Stop(); //Redundant?
+					shiftSource.clip = shiftClips[Random.Range(0, shiftClips.Length)];
+					shiftSource.Play();
+				}
+				if (shiftRechargeSource && shiftRechargeClips.Length > 0)
+				{
+					shiftRechargeSource.Stop(); //Redundant?
+					shiftRechargeSource.clip = shiftRechargeClips[Random.Range(0, shiftRechargeClips.Length)];
+					shiftRechargeSource.PlayScheduled(AudioSettings.dspTime + (cooldown - RECHARGE_SOUND_MARKER));
+				}
 
 				Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				mousePos.z = transform.position.z;
